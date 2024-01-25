b327d2689233125540dc6b268240047553feb230
Nerocraft4 <90830812+Nerocraft4@users.noreply.github.com>
2024-01-25 11:38:26 +0100

    Update KaminariTMOv3.py (minor edit)

--- KaminariTMOv3.py
+++ KaminariTMOv3.py
@@ -69,56 +69,55 @@ def base(img):
 
 imageio.plugins.freeimage.download()
 
-for num in range(4):
-    img = imageio.imread(r"./sample_images/scene"+str(num+1)+".hdr", format="HDR-FI")
-    print(np.max(img)/np.min(img[np.nonzero(img)])) #dynamic range
-    x,y,_ = img.shape
+img = imageio.imread(r"./sample_images/scene.hdr", format="HDR-FI")
+print(np.max(img)/np.min(img[np.nonzero(img)])) #dynamic range
+x,y,_ = img.shape
 
-    fiY = np.ndarray(shape=[x,y])
-    #extract color from initial image, X,Z values:
-    ogR,ogG,ogB = cv2.split(img)
-    ogXYZ = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-    ogX,ogY,ogZ = cv2.split(ogXYZ)
+fiY = np.ndarray(shape=[x,y])
+#extract color from initial image, X,Z values:
+ogR,ogG,ogB = cv2.split(img)
+ogXYZ = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+ogX,ogY,ogZ = cv2.split(ogXYZ)
 
-    #getting the expositions (?)
-    for i in range(4):
-        expo = np.clip(img*(2**12), 2**i, 2**(12-4+i))/2**(8+i)
-        XYZ = cv2.cvtColor(expo, cv2.COLOR_RGB2HLS)
-        X,Y,Z = cv2.split(XYZ)
-        print("image",i,np.min(Y),np.max(Y),np.average(Y))
-        Y255 = np.uint8(Y*255)
+#getting the expositions (?)
+for i in range(4):
+    expo = np.clip(img*(2**12), 2**i, 2**(12-4+i))/2**(8+i)
+    XYZ = cv2.cvtColor(expo, cv2.COLOR_RGB2HLS)
+    X,Y,Z = cv2.split(XYZ)
+    print("image",i,np.min(Y),np.max(Y),np.average(Y))
+    Y255 = np.uint8(Y*255)
 
-        values, labels = kmeans(data=Y,k=3)
-        img_segm = np.choose(labels, values)
-        img_segm.shape = [x,y]
-        # im = Image.fromarray(np.uint8(img_segm*255))
-        # im.show()
+    values, labels = kmeans(data=Y,k=3)
+    img_segm = np.choose(labels, values)
+    img_segm.shape = [x,y]
+    # im = Image.fromarray(np.uint8(img_segm*255))
+    # im.show()
 
-        Y_partialgamma = []
-        for i in range(len(Y)):
-            Y_partialgamma.append(gamma_clustered(Y[i],labels[i]))
-        Y_partialgamma = np.array(Y_partialgamma).reshape([x,y])
-        #im = Image.fromarray(np.uint8(Y_partialgamma*255))
-        fiY+=Y_partialgamma
+    Y_partialgamma = []
+    for i in range(len(Y)):
+        Y_partialgamma.append(gamma_clustered(Y[i],labels[i]))
+    Y_partialgamma = np.array(Y_partialgamma).reshape([x,y])
+    #im = Image.fromarray(np.uint8(Y_partialgamma*255))
+    fiY+=Y_partialgamma
 
-    fiY/=4 #average it
+fiY/=4 #average it
 
-    initial = img
-    im = Image.fromarray(np.uint8(initial*255))
-    #im.show()
-    cv2.imwrite("results/finalinitial"+str(num)+".jpg", cv2.cvtColor(np.uint8(initial*255), cv2.COLOR_RGB2BGR))
+initial = img
+im = Image.fromarray(np.uint8(initial*255))
+#im.show()
+cv2.imwrite("results/finalinitial.jpg", cv2.cvtColor(np.uint8(initial*255), cv2.COLOR_RGB2BGR))
 
-    #add a bit of saturation to compensate, these operations seem to desaturate a bit the image
-    fiZ = np.clip(ogZ*SATCONST,0,1)
+#add a bit of saturation to compensate, these operations seem to desaturate a bit the image
+fiZ = np.clip(ogZ*SATCONST,0,1)
 
-    final = cv2.merge([ogX, np.float32(fiY), fiZ])
-    final = cv2.cvtColor(final, cv2.COLOR_HLS2RGB)
-    im = Image.fromarray(np.uint8(final*255))
-    #im.show()
-    # cv2.imwrite("results/finalfinal"+str(num)+".jpg", cv2.cvtColor(np.uint8(final*255), cv2.COLOR_RGB2BGR))
+final = cv2.merge([ogX, np.float32(fiY), fiZ])
+final = cv2.cvtColor(final, cv2.COLOR_HLS2RGB)
+im = Image.fromarray(np.uint8(final*255))
+#im.show()
+# cv2.imwrite("results/finalfinal.jpg", cv2.cvtColor(np.uint8(final*255), cv2.COLOR_RGB2BGR))
 
-    #balancing to get darkest spots and recover some burnt features
-    mix = (final*6.5+initial*3.5)/10
-    im = Image.fromarray(np.uint8(mix*255))
-    #im.show()s
-    cv2.imwrite("results/finalmix"+str(num)+".jpg", cv2.cvtColor(np.uint8(mix*255), cv2.COLOR_RGB2BGR))
+#balancing to get darkest spots and recover some burnt features
+mix = (final*6.5+initial*3.5)/10
+im = Image.fromarray(np.uint8(mix*255))
+#im.show()s
+cv2.imwrite("results/finalmix".jpg", cv2.cvtColor(np.uint8(mix*255), cv2.COLOR_RGB2BGR))
